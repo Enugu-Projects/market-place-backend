a49a6713af642a73f7370ee6462e014c617d2319
alan <mmesomanwoko@gmail.com>
2019-12-03 14:26:48 +0000

    basic routes and api set up

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const express = require('express')
+const path = require('path')
+const logger = require('middleware/logger')
+
+//Initializing application
+app = express()
+
+
+
+/*-----------------------------------------------------------------------
+
+Middlewares
+
+--------------------------------------------------------------------------
+*/
+//Logger middleware
+app.use(logger)
+
+//Body Parser Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`))
